196dd22e68d589ff5656b6545486a0a3c8a2e96a
odeane_croney <odeane_croney@yahoo.com>
2020-10-30 15:39:27 +0100

    adding and using hbs handler

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ const app = express()
 
 const publicDirPath = path.join(__dirname, '../public') //manipulate the path
 
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Andrew Mead'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name:'Odeane'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'let me help',
+    name: 'Odeane says so'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
